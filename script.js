f7e85a60e9fead617b6ba3858ac99e66c0f96bb6
kemikal <janne@kemikal.org>
2023-10-18 11:58:18 +0200

    localStorage done

--- script.js
+++ script.js
@@ -4,22 +4,50 @@ let newTodo = document.getElementById("newTodo");
 let saveTodoBtn = document.getElementById("saveTodoBtn");
 let todoList = document.getElementById("todoList");
 
-let thingsToDo = ["Skriv ut lista", "lägg till nytt", "ta bort från listan", "Spara och gå hem"];
+if (localStorage.getItem("todos")) {
+    console.log("LS existerar");
+    
+} else {
+    console.log("Skapar LS");
+    let thingsToDo = ["Skriv ut lista", "lägg till nytt", "ta bort från listan", "Spara och gå hem"];
+    // SPARA
+    localStorage.setItem("todos", JSON.stringify(thingsToDo));
+  
+}
+
+
+
+
 
 function printList() {
 
     todoList.innerHTML = "";
+
+    // HÄMTA
+    let thingsToDo = JSON.parse(localStorage.getItem("todos"));
+    console.log("things from LS", thingsToDo);
 
     for (item of thingsToDo) {
         let li = document.createElement("li");
         li.innerText = item;
         li.id = item;
 
+        // TA BORT
         li.addEventListener("click", (e) => {
+
+            // HÄMTA 
+            let thingsToDo = JSON.parse(localStorage.getItem("todos"));
+            
+            // ÄNDRA 
+            thingsToDo.splice(thingsToDo.indexOf(e.target.id),1);
+            
+            // SPARA
+            localStorage.setItem("todos", JSON.stringify(thingsToDo));
+
             console.log("event", e.target.id);
             console.log("click på item");
 
-            thingsToDo.splice(thingsToDo.indexOf(e.target.id),1);
+            
             console.log("efter splice", thingsToDo);
             printList();
         })
@@ -29,7 +57,17 @@ function printList() {
 }
 
 saveTodoBtn.addEventListener("click", () => {
+
+    // HÄMTA
+    let thingsToDo = JSON.parse(localStorage.getItem("todos"));
+
+    // ÄNDRA 
     thingsToDo.push(newTodo.value);
+
+    // SPARA
+    localStorage.setItem("todos", JSON.stringify(thingsToDo));
+
+
     console.log("list after click", thingsToDo);
     newTodo.value = "";
     printList();
